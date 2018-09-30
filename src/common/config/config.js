6733e65a0b30213d4ebfc88b2feb54db1351bd86
fuyi501 <2516800961@qq.com>
2018-09-30 21:59:26 +0800

    https

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -2,6 +2,21 @@
 /**
  * config
  */
-export default {
+
   //key: value
-};
+  import fs from 'fs';
+  import https from 'https';
+  const options = {
+    key: fs.readFileSync('./ssl/server.key'),
+    cert: fs.readFileSync('./ssl/server.crt')
+  };
+  const app = (callback, port, host, think) => {
+    let server = https.createServer(options, callback);
+    server.listen(port, host);
+    // console.log('server', server)
+    return server;
+  }
+  export default {
+    // port: 9000, //服务监听的端口
+    create_server: app
+  };
