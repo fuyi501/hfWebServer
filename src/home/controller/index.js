a85e1dd8859316f7b5f38c381d4391e38e056f89
fuyi501 <2516800961@qq.com>
2018-10-09 17:26:47 +0800

    待完成人脸库更新

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -3,16 +3,18 @@
 import Base from './base.js';
 
 const fs = require('fs');
+var exec = require('child_process').exec;
+
 export default class extends Base {
   /**
    * index action
    * @return {Promise} []
    */
-  indexAction(){
+  indexAction() {
     //auto render template file index_index.html
     return this.display();
   }
-  setCorsHeader(){
+  setCorsHeader() {
     this.header('Access-Control-Allow-Origin', this.header('origin') || '*');
     this.header('Access-Control-Allow-Headers', 'Content-Type');
     // this.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
@@ -23,52 +25,192 @@ export default class extends Base {
     this.setCorsHeader()
     if (this.isPost()) {
       let data = this.post()
-      console.log("data:",data)
+      console.log("data:", data)
       if (!think.isEmpty(data.tableInfo)) {
-        if(data.tableType === 'staff'){
-          let res = await this.model("teststaff").addMany(data.tableInfo)
-          console.log('res:',res)
-          return this.jsonp({code:2000,desc: "添加成功"})
-        }else if(data.tableType === 'schedule'){
-          let res = await this.model("testschedule").addMany(data.tableInfo[i])
-          console.log('res:',res)
-          return this.jsonp({code:2000,desc: "添加成功"})
-        }else {
-          return this.jsonp({code:2001,desc: "添加字段不对"})
+        if (data.tableType === 'staff') {
+          try {
+            for (let i in data.tableInfo) {
+              let res = await this.model("teststaff").add(data.tableInfo[i])
+              console.log('res:', res)
+            }
+            // let res = await this.model("teststaff").addMany(data.tableInfo)
+            return this.jsonp({
+              code: 2000,
+              desc: "添加成功"
+            })
+          } catch (error) {
+            console.log(error)
+            return this.jsonp({
+              code: 2002,
+              desc: error
+            })
+          }
+        } else if (data.tableType === 'schedule') {
+          try {
+            for (let i in data.tableInfo) {
+              let res = await this.model("testschedule").add(data.tableInfo[i])
+              console.log('res:', res)
+            }
+            return this.jsonp({
+              code: 2000,
+              desc: "添加成功"
+            })
+          } catch (error) {
+            console.log(error)
+            return this.jsonp({
+              code: 2002,
+              desc: error
+            })
+          }
+
+        } else {
+          return this.jsonp({
+            code: 2001,
+            desc: "添加字段不对"
+          })
         }
       }
-    }else {
+    } else {
       this.fail('请求的方法不对')
     }
   }
   // 保存拍摄的图片
-  async imgAction(){
+  async imgAction() {
     console.log("保存拍摄的图片")
     this.setCorsHeader()
     if (this.isPost()) {
       let imgInfo = this.post()
       // console.log("imgInfo:",imgInfo)
       imgInfo = imgInfo.imgInfo
-      if(!think.isEmpty(imgInfo.name)){
-        
+      if (!think.isEmpty(imgInfo.name)) {
+
         //接收前台POST过来的base64
         var imgData = imgInfo.url
         //过滤data:URL
         var base64Data = imgData.replace(/^data:image\/\w+;base64,/, "")
         var dataBuffer = new Buffer(base64Data, 'base64')
+        // let savepath = '/DATACENTER3/huifu/HuiFu_Project/staff_photo/' + imgInfo.name
         let savepath = think.RESOURCE_PATH + '/upload/faceimg/' + imgInfo.name
-        fs.exists(savepath, (isexist) => {
-          console.log("isexist", isexist)
-          if(!isexist){
-            fs.writeFile(savepath, dataBuffer, (err) => {
-              console.log("保存成功")
-              return this.jsonp({code:2000,desc: "添加成功"})
+
+        let faceInfo = imgInfo.name.split('_')
+        let faceCount = faceInfo[2].split('.')
+        console.log(faceInfo, faceCount)
+
+        let savepath1 = '/DATACENTER3/huifu/HuiFu_Project/staff_photo/' + faceInfo[0] + '_' + faceInfo[1] + '.jpg'
+        let savepath2 = '/DATACENTER3/huifu/face_lib/' + faceInfo[0]
+
+        // 第一张保存
+        if (faceCount[0] === '1') {
+          fs.exists(savepath1, (isexist) => {
+            console.log("isexist", isexist)
+            if (!isexist) {
+              fs.writeFile(savepath1, dataBuffer, (err) => {
+                console.log("不存在，准备保存")
+                if (err === null) {
+                  console.log(imgInfo.name + " 保存成功")
+                  return this.jsonp({
+                    code: 2000,
+                    desc: imgInfo.name + " 添加成功"
+                  })
+                } else {
+                  console.log(imgInfo.name + " 保存失败", err)
+                  return this.jsonp({
+                    code: 2002,
+                    desc: imgInfo.name + " 添加失败"
+                  })
+                }
+              })
+            } else {
+              console.log(imgInfo.name + " 文件已存在")
+              return this.jsonp({
+                code: 2001,
+                desc: imgInfo.name + " 文件已存在"
+              })
+            }
+          })
+        }
+
+        // 其他的保存
+        fs.exists(savepath2, (isexist) => {
+          let savafacepath = savepath2 + '/' + (Number(faceCount[0]) - 1) + '.jpg'
+          if (!isexist) {
+            console.log("不存在，创建目录")
+            fs.mkdir(savepath2, (err) => {
+              if (!err) {
+                console.log('创建成功')
+                fs.chmod(savepath2, '0777', (err) => {
+                  if (err) {
+                    console.log("修改失败")
+                  } else {
+                    console.log("修改权限成功")
+                    fs.exists(savafacepath, (isexist) => {
+                      console.log("isexist", isexist)
+                      if (!isexist) {
+                        fs.writeFile(savafacepath, dataBuffer, (err) => {
+                          if (!err) {
+                            console.log(imgInfo.name + " 保存成功")
+                            return this.jsonp({
+                              code: 2000,
+                              desc: imgInfo.name + " 添加成功"
+                            })
+                          } else {
+                            console.log(imgInfo.name + " 保存失败", err)
+                            return this.jsonp({
+                              code: 2002,
+                              desc: imgInfo.name + " 添加失败"
+                            })
+                          }
+                        })
+                      } else {
+                        console.log(imgInfo.name + " 文件已存在")
+                        return this.jsonp({
+                          code: 2001,
+                          desc: imgInfo.name + " 文件已存在"
+                        })
+                      }
+                    })
+                  }
+                })
+              } else {
+                return this.jsonp({
+                  code: 2005,
+                  desc: '创建目录错误：' + err
+                })
+              }
             })
-          }else{
-            console.log("文件已存在")
-            return this.jsonp({code:2001,desc: "文件已存在"})
+          } else {
+            console.log("路径存在", savafacepath)
+            fs.exists(savafacepath, (isexist) => {
+              console.log("isexist", isexist)
+              if (!isexist) {
+                fs.writeFile(savafacepath, dataBuffer, (err) => {
+                  if (!err) {
+                    console.log(imgInfo.name + " 保存成功")
+                    return this.jsonp({
+                      code: 2000,
+                      desc: imgInfo.name + " 添加成功"
+                    })
+                  } else {
+                    console.log(imgInfo.name + " 保存失败", err)
+                    return this.jsonp({
+                      code: 2002,
+                      desc: imgInfo.name + " 添加失败"
+                    })
+                  }
+                })
+              } else {
+                console.log(imgInfo.name + " 文件已存在")
+                return this.jsonp({
+                  code: 2001,
+                  desc: imgInfo.name + " 文件已存在"
+                })
+              }
+            })
           }
         })
+
+
+
       }
     } else {
       this.fail('请求方法不对')
@@ -78,34 +220,201 @@ export default class extends Base {
   async saveimgAction() {
     this.setCorsHeader()
     if (this.isPost()) {
-        let userInfo = this.post()
-        console.log("userInfo:",userInfo)
-        let userAvatar = this.file('avatar')
-        console.log(userAvatar)
-        if (userAvatar.originalFilename) {
-          fs.readFile(userAvatar.path, (err, data) => {
+      let userInfo = this.post()
+      console.log("userInfo:", userInfo)
+      let userAvatar = this.file('avatar')
+      console.log(userAvatar)
+      if (userAvatar.originalFilename) {
+        fs.readFile(userAvatar.path, (err, data) => {
 
-            // var imgType = userAvatar.originalFilename.split('.')[1]
-            // var newName = userInfo.uid  + '.' + imgType
-            var savePath = think.RESOURCE_PATH + '/upload/avatar/' + userAvatar.originalFilename
-            fs.exists(savePath, (isexist) => {
-              console.log("isexist", isexist)
-              if(!isexist){
-                fs.writeFile(savePath, data, (err) => {
+          // var imgType = userAvatar.originalFilename.split('.')[1]
+          // var newName = userInfo.uid  + '.' + imgType
+          var savePath = think.RESOURCE_PATH + '/upload/avatar/' + userAvatar.originalFilename
+          fs.exists(savePath, (isexist) => {
+            console.log("是否已存在：", isexist)
+            if (!isexist) {
+              fs.writeFile(savePath, data, (err) => {
+                console.log("不存在，准备保存")
+                if (err === null) {
                   console.log("保存成功")
-                  return this.jsonp({code:2000,desc: "添加成功"})
-                })
-              }else{
-                console.log("文件已存在")
-                return this.jsonp({code:2001,desc: "文件已存在"})
-              }
-            })
+                  return this.jsonp({
+                    code: 2000,
+                    desc: "添加成功"
+                  })
+                } else {
+                  console.log("保存失败")
+                  return this.jsonp({
+                    code: 2000,
+                    desc: err
+                  })
+                }
+
+              })
+            } else {
+              console.log("文件已存在")
+              return this.jsonp({
+                code: 2001,
+                desc: "文件已存在"
+              })
+            }
           })
-        }else {
-          return this.jsonp({code:2002,desc: "文件格式错误"})
-        }
+        })
+      } else {
+        return this.jsonp({
+          code: 2002,
+          desc: "文件格式错误"
+        })
+      }
     } else {
-        this.fail('请求方法不对')
+      this.fail('请求方法不对')
     }
-}
+  }
+  // 获取表格数据
+  async gettableAction() {
+    this.setCorsHeader()
+    let data = this.get()
+    console.log(data)
+    if (data.type === 'sche') {
+      data = await this.model('testschedule').select();
+      console.log("data:", data);
+      return this.success(data);
+    } else if (data.type === 'staff') {
+      data = await this.model('teststaff').select();
+      console.log("data:", data);
+      return this.success(data);
+    } else {
+      this.fail('请求方法不对')
+    }
+  }
+  //删除表格数据
+  async deletetableAction() {
+    this.setCorsHeader()
+    if (this.isPost()) {
+      let data = this.post()
+      console.log(data)
+      if (data.type === 'sche') {
+        let res = await this.model("testschedule").where({
+          t_id: data.tableData.t_id
+        }).delete(data.tableData);
+        console.log('res:', res)
+        console.log('更新成功')
+        this.success({
+          code: 2000,
+          desc: "更新成功"
+        })
+      } else if (data.type === 'staff') {
+        let res = await this.model("teststaff").where({
+          staff_id: data.tableData.staff_id
+        }).delete(data.tableData);
+        console.log('res:', res)
+        console.log('更新成功')
+        this.success({
+          code: 2000,
+          desc: "更新成功"
+        })
+      } else {
+        this.fail('请求参数不对')
+      }
+    } else {
+      this.fail('请求方法不对')
+    }
+  }
+  //编辑表格信息
+  async edittableAction() {
+    this.setCorsHeader()
+    if (this.isPost()) {
+      let data = this.post()
+      console.log(data)
+      if (data.type === 'sche') {
+        let res = await this.model("testschedule").where({
+          t_id: data.tableData.t_id
+        }).update(data.tableData);
+        console.log('res:', res)
+        console.log('更新成功')
+        this.success({
+          code: 2000,
+          desc: "更新成功"
+        })
+      } else if (data.type === 'staff') {
+        let res = await this.model("teststaff").where({
+          staff_id: data.tableData.staff_id
+        }).update(data.tableData);
+        console.log('res:', res)
+        console.log('更新成功')
+        this.success({
+          code: 2000,
+          desc: "更新成功"
+        })
+      } else {
+        this.fail('请求参数不对')
+      }
+    } else {
+      this.fail('请求方法不对')
+    }
+  }
+  //添加表格信息
+  async addtableAction() {
+    this.setCorsHeader()
+    if (this.isPost()) {
+      let data = this.post()
+      console.log(data)
+      if (data.type === 'sche') {
+        let res = await this.model("testschedule").add(data.tableData);
+        console.log('res:', res)
+        console.log('更新成功')
+        this.success({
+          code: 2000,
+          desc: "更新成功"
+        })
+      } else if (data.type === 'staff') {
+        let res = await this.model("teststaff").add(data.tableData);
+        console.log('res:', res)
+        console.log('更新成功')
+        this.success({
+          code: 2000,
+          desc: "更新成功"
+        })
+      } else {
+        this.fail('请求参数不对')
+      }
+    } else {
+      this.fail('请求方法不对')
+    }
+  }
+  //更新人脸库
+  async updatefaceAction() {
+    console.log('更新人脸库')
+    this.setCorsHeader()
+    if (this.isPost()) {
+      console.log('更新人脸库')
+      let data = this.post()
+      console.log(data)
+      if (data.faceIds.length > 0) {
+
+        let filename = '/DATACENTER3/huifu/HuiFu_Project/update_face_lib/update_face_lib.py'
+        exec('python ' + filename, function (err, stdout, stdin) {
+          if (err) {
+            console.log('err', err)
+          }
+          if (stdout) {
+            // parse the string
+            console.log(stdout)
+          }
+        });
+
+        console.log('更新成功')
+        this.success({
+          code: 2000,
+          desc: "更新成功"
+        })
+      } else {
+        this.success({
+          code: 2001,
+          desc: "没有需要更新的"
+        })
+      }
+    } else {
+      this.fail('请求方法不对')
+    }
+  }
 }
